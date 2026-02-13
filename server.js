a84526c1e6c4da0708b687f9e7cc5b96dba2ca7c
sanku5003 <pariharsanket10@gmail.com>
2026-02-13 14:37:52 +0530

    plan changed

--- server.js
+++ server.js
@@ -15,10 +15,6 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const User = require('./models/User.js');
 const upload = multer({ dest: 'uploads/' });
-const Basic = require("./models/webPage/basic.js");
-const About = require("./models/webPage/about.js");
-const management = require("./models/webPage/management.js");
-const Event = require("./models/webPage/events.js");
 
 let port = 8080;
 
@@ -64,9 +60,9 @@ app.use((req, res, next) => {
     next();
 });
 
-// app.get("/", (req, res) => {
-//   res.send("Hi, I am root");
-// });
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+});
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -75,6 +71,10 @@ passport.use(new Localstrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+
+app.get("/repulsor/register", (req, res) => {
+    res.render("signup.ejs");
+})
 
 app.post('/repulsor/register', async (req, res) => {
     try {
@@ -112,255 +112,8 @@ app.post('/repulsor/register', async (req, res) => {
     }
 });
 
-app.get("/repulsor/register", (req, res) => {
-    res.render("signup.ejs");
-})
-
-app.get('/repulsor/:id/basic', async (req, res) => {
-    let { id } = req.params;
-    const user = User.findById(id);
-    res.render('webPage/basic.ejs', { user });
-})
-
-app.post(
-    '/repulsor/:id/basic',
-    upload.fields([
-        { name: 'logo.url', maxCount: 1 },
-        { name: 'cover.url', maxCount: 1 }
-    ]),
-    async (req, res) => {
-        try {
-            const { id } = req.params;
-
-            const {
-                slogan,
-                num1, text1,
-                num2, text2,
-                num3, text3
-            } = req.body;
-
-            const logoFile = req.files['logo.url']?.[0];
-            const coverFile = req.files['cover.url']?.[0];
-
-            const basicData = new Basic({
-                slogan,
-                num1,
-                text1,
-                num2,
-                text2,
-                num3,
-                text3,
-                owner: id,
-                logo: logoFile
-                    ? {
-                        filename: logoFile.filename,
-                        url: `/uploads/${logoFile.filename}`
-                    }
-                    : undefined,
-                cover: coverFile
-                    ? {
-                        filename: coverFile.filename,
-                        url: `/uploads/${coverFile.filename}`
-                    }
-                    : undefined
-            });
-
-            await basicData.save();
-
-            req.flash("success", "Basic details saved successfully 🚀");
-            res.redirect(`/repulsor/${id}/about`);
-
-        } catch (err) {
-            console.error(err);
-            req.flash("error", "Something went wrong while saving details");
-            res.redirect(`/repulsor/${id}/basic`);
-        }
-    }
-);
-
-app.get("/repulsor/:id/about", (req, res) => {
-    let { id } = req.params;
-    res.render("webPage/about.ejs");
-})
-
-app.post("/repulsor/:id/about", async (req, res) => {
-    try {
-        const { id } = req.params;
-        let { aboutBrief, about } = req.body;
-        let aboutData = new About({
-            aboutBrief,
-            about,
-            owner: id
-        })
-
-        aboutData.save();
-        res.redirect(`/repulsor/${id}/management`);
-    } catch {
-
-        req.flash("error", "Something went wrong while saving details");
-        res.redirect(`/repulsor/${id}/about`);
-    }
-
-})
-
-app.get('/repulsor/:id/management', (req, res) => {
-    let { id } = req.params;
-    res.render('webpage/management.ejs');
-})
-
-// app.post('/repulsor/:id/management', async (req, res) => {
-//     try {
-
-//         let team = new management({
-//             member: req.body.member,
-//             owner: id
-//         })
-//         await team.save();
-//         res.redirect(`/repulsor/${id}/events`);
-//     } catch {
-//         let { id } = req.params;
-//         req.flash("error", "Something went wrong while saving details");
-//         res.redirect(`/repulsor/${id}/management`);
-//     }
 
 
-// })
-
-
-app.post(
-    '/repulsor/:id/management',
-    upload.any(),
-    async (req, res) => {
-        try {
-            let { id } = req.params;
-
-            // member text data
-            const memberBody = req.body.member[0];
-
-            // photo file
-            const photoFile = req.files.find(
-                f => f.fieldname === 'member[0][photo.url]'
-            );
-
-            const team = new management({
-                member: [
-                    {
-                        Role: memberBody.Role,
-                        Name: memberBody.Name,
-                        age: memberBody.age,
-                        Gender: memberBody.Gender,
-                        Qualification: memberBody.Qualification,
-                        photo: photoFile
-                            ? {
-                                filename: photoFile.filename,
-                                url: `/uploads/${photoFile.filename}`
-                            }
-                            : undefined
-                    }
-                ],
-                owner: id
-            });
-
-            await team.save();
-            res.redirect(`/repulsor/${id}/events`);
-
-        } catch (err) {
-            console.error(err);
-            req.flash("error", "Something went wrong while saving details");
-            res.redirect(`/repulsor/${id}/management`);
-        }
-    }
-);
-
-
-app.get('/repulsor/:id/events', (req, res) => {
-    let { id } = req.params;
-    res.render('webPage/events.ejs');
-})
-
-// app.post('/repulsor/:id/events',  async (req, res) => {
-//     try {
-//         let { id } = req.params;
-//         let eventData = new Event({
-//             event: req.body.event,
-//             owner: id
-//         })
-
-//         await eventData.save();
-//         res.redirect(`/repulsor/${id}/home`);
-//     } catch {
-//         let { id } = req.params;
-//         req.flash("error", "Something went wrong while saving details");
-
-//         res.redirect(`/repulsor/${id}/events`);
-//     }
-// })
-
-
-app.post(
-    '/repulsor/:id/events',
-    upload.any(),
-    async (req, res) => {
-        try {
-            let { id } = req.params;
-
-            const eventBody = req.body.event[0];
-
-            // identify files manually
-            const primaryFile = req.files.find(
-                f => f.fieldname === 'event[0][primaryPhoto.url]'
-            );
-
-            const secondaryFile = req.files.find(
-                f => f.fieldname === 'event[0][secondaryPhoto.url]'
-            );
-
-            const eventData = new Event({
-                event: [
-                    {
-                        Name: eventBody.Name,
-                        purpose: eventBody.purpose,
-                        about: eventBody.about,
-                        primaryPhoto: primaryFile
-                            ? {
-                                filename: primaryFile.filename,
-                                url: `/uploads/${primaryFile.filename}`
-                            }
-                            : undefined,
-                        secondaryPhoto: secondaryFile
-                            ? {
-                                filename: secondaryFile.filename,
-                                url: `/uploads/${secondaryFile.filename}`
-                            }
-                            : undefined
-                    }
-                ],
-                owner: id
-            });
-
-            await eventData.save();
-            res.redirect(`/repulsor/${id}/home`);
-
-        } catch (err) {
-            console.error(err);
-            req.flash("error", "Something went wrong while saving details");
-            res.redirect(`/repulsor/${id}/events`);
-        }
-    }
-);
-
-
-
-app.get('/repulsor/:id/home', async(req, res) => {
-    let { id } = req.params;
-    const user = await User.findById(id);
-    const basicData = await Basic.findOne({owner : id});
-    const aboutData = await About.findOne({owner : id});
-    const managementData = await management.find({owner : id});
-    const EventData = await Event.find({owner : id});
-    
-    res.render("webPage/webPage.ejs" , {user , basicData , aboutData , managementData , EventData});
-})
 
 
 app.listen(port, () => {
